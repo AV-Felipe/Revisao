9d28c890828052745683ae7eaad612718b114ed7
AV-Felipe <aerogrado@gmail.com>
2021-05-19 16:20:38 -0300

    menu de opções convertido em método

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -6,6 +6,35 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            string opcaoUsurio = obterOpcaoUsuario();
+
+            while (opcaoUsurio.ToUpper() != "X")
+            {
+                switch (opcaoUsurio)
+                {
+                    case "1":
+                        //TODO: cadastro de aluno
+                        break;
+
+                    case "2":
+                        //TODO: listagem de alunos
+                        break;
+
+                    case "3":
+                        //TODO: cálculo de média
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsurio = obterOpcaoUsuario();
+
+            }
+        }
+
+        private static string obterOpcaoUsuario()
+        {
             Console.WriteLine("ESCOLHA UMA OPÇÃO:");
             Console.WriteLine("1 - Inserir novo aluno;");
             Console.WriteLine("2 - Listar alunos;");
@@ -14,37 +43,7 @@ namespace Revisao
             Console.WriteLine();
 
             string opcaoUsurio = Console.ReadLine();
-
-            while(opcaoUsurio.ToUpper() != "X")
-            {
-                switch(opcaoUsurio)
-                {
-                    case "1":
-
-                        break;
-
-                    case "2":
-
-                        break;
-
-                    case "3":
-
-                        break;
-                    
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                Console.WriteLine("ESCOLHA UMA OPÇÃO:");
-                Console.WriteLine("1 - Inserir novo aluno;");
-                Console.WriteLine("2 - Listar alunos;");
-                Console.WriteLine("3 - Calcular média geral;");
-                Console.WriteLine("X - Sair");
-                Console.WriteLine();
-
-                opcaoUsurio = Console.ReadLine();
-
-            }
+            return opcaoUsurio;
         }
     }
 }
