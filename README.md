f64f461f739c23431f258820d408bd25314bb870
AV-Felipe <aerogrado@gmail.com>
2021-05-19 15:47:32 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+ # Revisão de conteúdo de introdução ao C#
+ 
+Programa em console, com o propósito de exercitar o aprendido na disciplina de introdução ao C#, do DIO, ministrada por Gabriel Faraday (https://about.me/gabrielfaraday).
+
+O programa terá as seguintes funcionalidades:
+
+ - cadastro de alunos e suas notas
+ - relatório de alunos e suas notas
+ - apuração de média geral da turma
---
+++ Revisao/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ESCOLHA UMA OPÇÃO:");
+            Console.WriteLine("1 - Inserir novo aluno;");
+            Console.WriteLine("2 - Listar alunos;");
+            Console.WriteLine("3 - Calcular média geral;");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsurio = Console.ReadLine();
+
+            while(opcaoUsurio.ToUpper() != "X")
+            {
+                switch(opcaoUsurio)
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("ESCOLHA UMA OPÇÃO:");
+                Console.WriteLine("1 - Inserir novo aluno;");
+                Console.WriteLine("2 - Listar alunos;");
+                Console.WriteLine("3 - Calcular média geral;");
+                Console.WriteLine("X - Sair");
+                Console.WriteLine();
+
+                opcaoUsurio = Console.ReadLine();
+
+            }
+        }
+    }
+}
